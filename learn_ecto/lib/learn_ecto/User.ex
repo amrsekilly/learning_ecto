4809d1892f5b4c383f1a595884e7a519350eed64
Amr Elsekilly <github@amrsekilly.com>
2018-10-30 01:47:29 +0200

    Added a schema

---
+++ learn_ecto/lib/learn_ecto/User.ex
@@ -0,0 +1,25 @@
+defmodule ExampleApp.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field(:username, :string)
+    field(:encrypted_password, :string)
+    field(:email, :string)
+    field(:confirmed, :boolean, default: false)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+
+    timestamps
+  end
+
+  @required_fields ~w(username encrypted_password email)
+  @optional_fields ~w()
+
+  def changeset(user, params \\ :empty) do
+    user
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> unique_constraint(:username)
+  end
+end
